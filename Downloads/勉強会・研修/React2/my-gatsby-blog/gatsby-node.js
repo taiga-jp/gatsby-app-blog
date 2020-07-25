1619b8de1471f94d1588ee299975d03e85954b88
Taiga Yamamoto <taiga.y1133@gmail.com>
2020-07-25 21:23:08 +0900

    first commit

---
+++ Downloads/勉強会・研修/React2/my-gatsby-blog/gatsby-node.js
@@ -0,0 +1,46 @@
+/**
+ * Implement Gatsby's Node APIs in this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/node-apis/
+ */
+
+// You can delete this file if you're not using it
+
+const path = require(`path`)
+const slash = require(`slash`)
+exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions
+  return graphql(
+    `
+      {
+        allContentfulBlogPost {
+          edges {
+            node {
+              id
+              slug
+            }
+          }
+        }
+      }
+    `
+  )
+    .then(result => {
+      if (result.errors) {
+        console.log("Error retrieving contentful data", result.errors)
+      }
+      const blogPostTemplate = path.resolve("./src/templates/blogpost.js")
+      result.data.allContentfulBlogPost.edges.forEach(edge => {
+        createPage({
+          path: `/blogpost/${edge.node.slug}/`,
+          component: slash(blogPostTemplate),
+          context: {
+            slug: edge.node.slug,
+            id: edge.node.id,
+          },
+        })
+      })
+    })
+    .catch(error => {
+      console.log("Error retrieving contentful data", error)
+    })
+}
